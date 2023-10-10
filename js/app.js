f84cc08c79585a96508525b737c660d56f8ce586
CalinCretu <calincretu1412@gmail.com>
2023-10-11 01:00:38 +0200

    ADD - img_null_error

--- js/app.js
+++ js/app.js
@@ -1,66 +1,118 @@
-const imagesDOMElement = [
+const images = [
   'img/01.webp',
   'img/02.webp',
   'img/03.webp',
   'img/04.webp',
   'img/05.webp'
 ];
+const slider = document.querySelector('.image-list');
+let currentImage = 0;
+console.log(currentImage)
 
-const galleryDOMElements = document.querySelector('.image-list');
-let i = 0;
-const currentSrc = imagesDOMElement[0]
-
-let botBtnDOMElement = document.getElementById('next-slide');
-
-let topBtnDOMElement = document.getElementById('prev-slide');
-
-
-for (let i = 0; i < imagesDOMElement.length; i++) {
-  const currentSrc = imagesDOMElement[i]
-  const htmlString = `
-    <div class="img">
-      <img src="${currentSrc}">
-    </div>
-    `
-  galleryDOMElements.innerHTML += htmlString
-  let imgDOMElement = document.querySelector('.img');
-  imgDOMElement.classList.add('active')
+function changeImage(index) {
+  let image = slider.querySelector('.hidden:nth-child(1)');
+  image.src = images[index];
+  image.classList.add('active');
+  image.classList.remove('hidden');
+  console.log(images[index])
 }
 
-botBtnDOMElement.addEventListener('click', function () {
-  for (let i = 0; i < imagesDOMElement.length; i++) {
-    const currentSrc = imagesDOMElement[i]
-    const htmlString = `
-      <div class="img">
-        <img src="${currentSrc}">
-      </div>
-      `
-    galleryDOMElements.innerHTML += htmlString
-    let imgDOMElement = document.querySelector('.img');
-    imgDOMElement.classList.add('active')
-    console.log(botBtnDOMElement)
+for (let i = 0; i < images.length; i++) {
+  const img = document.createElement('img');
+  img.src = images[i];
+  img.classList.add('hidden');
+  slider.appendChild(img);
+}
+
+let firstImage = slider.querySelector('.hidden:nth-child(1)');
+firstImage.classList.remove("hidden");
+console.log(firstImage)
+
+const prevButton = document.getElementById('prev-slide');
+prevButton.addEventListener('click', function () {
+  currentImage--;
+  console.log(currentImage)
+  if (currentImage < 0) {
+    currentImage = images.length - 1;
   }
-})
+  changeImage(currentImage);
+});
+
+const nextButton = document.getElementById('next-slide');
+nextButton.addEventListener('click', function () {
+  currentImage++;
+  if (currentImage >= images.length) {
+    currentImage = 0;
+  }
+  changeImage(currentImage);
+});
 
 
 
 
 
-// botBtnDOMElement.addEventListener('click', function () {
-//   imageDOMElement1.classList.remove('active');
-//   imageDOMElement2.classList.add('active');
-// })
-
-// topBtnDOMElement.addEventListener('click', function () {
-//   imageDOMElement1.classList.add('active');
-//   imageDOMElement2.classList.remove('active');
-// })
 
 
 
 
-// const imageDOMElement = galleryDOMElements[0 + costante
-//   const imageDOMElement = galleryDOMElements[1]
-//   imageDOMElement.classList.add('active')
 
-//  abbiamo la gallery. quando abbiamo l'elemento 0 della gallery dobbiamo dargli active. if gallerydomelement = 0 diamo active. if gallerydomelement = 1 diamo active e togliamo active a quello prima.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const galleryDOMElements = document.querySelector('.image-list');
+// let i = 0;
+// const currentSrc = images[i]
+
+// let botBtnDOMElement = document.getElementById('next-slide');
+
+// let topBtnDOMElement = document.getElementById('prev-slide');
+
+
+// // for (let i = 0; i < images.length; i++) {
+// //   const currentSrc = images[i]
+// const htmlString = `
+//   <div class="img">
+//     <img src="${currentSrc}">
+//   </div>
+//   `
+// console.log(currentSrc)
+// galleryDOMElements.innerHTML += htmlString
+// const imgDOMElement = document.querySelector('.img');
+// imgDOMElement.classList.add('active')
+// for (let i = 0; i < images.length; i++) {
+//   botBtnDOMElement.addEventListener('click', function () {
+//     if (i = 1) {
+//       imgDOMElement.classList.remove('active')
+//       console.log(currentSrc)
+//     }
+//   })
+// }
+
+
+
+
+
+
+
+
+
